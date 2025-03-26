704eac1d19573267208795a71357fe816d3f2c36
daveprowse <dprowseus@gmail.com>
2025-03-26 15:32:56 -0400

    cursor-issue

--- area.js
+++ area.js
@@ -885,9 +885,9 @@ export const DrawingArea = GObject.registerClass({
         else if (this.hasManipulationTool)
             this.setPointerCursor(this.grabbedElement ? 'MOVE_OR_RESIZE_WINDOW' : 'DEFAULT');
         else if (this.currentElement && this.currentElement.shape == Shape.TEXT && this.isWriting)
-            this.setPointerCursor('IBEAM');
+            this.setPointerCursor('TEXT');
         else if (!this.currentElement)
-            this.setPointerCursor(this.currentTool == Shape.NONE ? 'POINTING_HAND' : 'CROSSHAIR');
+            this.setPointerCursor(this.currentTool == Shape.NONE ? 'POINTER' : 'CROSSHAIR');
         else if (this.currentElement.shape != Shape.NONE && controlPressed)
             this.setPointerCursor('MOVE_OR_RESIZE_WINDOW');
     }
